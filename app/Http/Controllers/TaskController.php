9bce047a016e53d3d6c6ab491cc6ea315bde2c3f
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2025-09-04 13:47:39 +0300

    feat: tasks controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks/index', ['tasks' => $tasks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $task = new Task();
+        return view('tasks/form', ['task' => $task]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|min:3|max:100',
+            'description' => 'required|min:3|max:255',
+            'status' => 'required|in:pending,completed'
+        ]);
+
+        Task::create($validated);
+        return redirect(route('tasks.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $task = Task::findOrFail($id);
+        return view('tasks/show', ['task' => $task]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $task = Task::findOrFail($id);
+        return view('tasks/form', ['task' => $task]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'title' => 'required|min:3|max:100',
+            'description' => 'required|min:3|max:255',
+            'status' => 'required|in:pending,completed'
+        ]);
+
+        $task = Task::findOrFail($id);
+        $task->update($validated);
+        $task->save();
+        return redirect(route('tasks.index'));
+    }
+
+    public function updateStatus(Request $request, string $id)
+    {
+        $task = Task::findOrFail($id);
+        $validated = $request->validate([
+            'status' => 'required|in:pending,completed'
+        ]);
+        $task->update($validated);
+        $task->save();
+        return redirect(route('tasks.show', $id));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return redirect()->route('tasks.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('tasks.index'));
 });
+
+Route::resource('tasks', TaskController::class);
+Route::put('/tasks/{id}/status', [TaskController::class, 'updateStatus'])->name('tasks.updateStatus');
